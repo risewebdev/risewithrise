453464816f2ba8e28c1328941e3061a728a7af78
Tabish <taabishm2@gmail.com>
2018-08-02 07:55:43 +0530

    First upload

---
+++ blog/includes/config.php
@@ -0,0 +1,10 @@
+<?php
+
+	define("DB_HOST" , "localhost");
+	define("DB_USER" , "root");
+	define("DB_PASS" , "xtw9reCj3svjBumX");
+	define("DB_NAME" , "blogcms");
+	
+	$about_text = "Danyal Fayyaz sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.";
+	$page_title = "Home | BlogCMS";
+?>
---
+++ graph/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/ettrics/pen/jERWPP.
+
+ An interactive bar graph packed with animations and information. Built with CSS and jQuery.
